7c10d75884c12af1dc2dfc5488e6b178f5c2322b
Muchael123 <mainamichael2849@gmail.com>
2024-11-14 19:43:12 +0300

    Project init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='min-h-screen w-screen bg-[#00072d] text-white'>
+      hello world
+    </div>
+  )
+}
+
+export default App
